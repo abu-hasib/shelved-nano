a22e6a6974cab51ab5726a19cd62f0a25cee6aac
Ridwan <ricklick262@gmail.com>
2021-08-06 13:02:27 +0100

    fix: Separate authors with commas and spacing

--- src/components/Books/Book.js
+++ src/components/Books/Book.js
@@ -28,7 +28,9 @@ function Book(props) {
           </div>
         </div>
         <div className="book-title">{book.title}</div>
-        <div className="book-authors">{book.authors}</div>
+        <div className="book-authors">
+          {book.authors && book.authors.join(", ")}
+        </div>
       </div>
     </li>
   );
